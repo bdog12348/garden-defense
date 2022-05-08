929c2b71961bd1f6d2ee19cbc0dd96f538cb50cf
Mindy Jun <mindyjvn@gmail.com>
2022-05-08 15:57:14 -0700

    WIP Swapping Weapons
    Successfully swaps, but only once
    
    Problems:
    - for some reason, can only hold a weapon once (can't pick it back up again)
    - infinite pickup range, don't even have to be looking at the weapon

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -8,35 +8,49 @@ public class PickUp : MonoBehaviour
     public GameObject hitbox;
     public Transform hitboxLocation;
     [SerializeField] StarterAssets.StarterAssetsInputs inputs;
+
+    private Transform prevWep;
     
     void Update() 
     {
-        if (inputs.pickup)  //E
+        if (inputs.pickup) 
         {
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().freezeRotation = true;
-            this.transform.position = d.position;
-            this.transform.parent = GameObject.Find("WepSlot").transform;
-            Debug.Log("PickingUp");
-            inputs.pickup = false;
-            hitbox.transform.parent = hitboxLocation;
-            hitbox.transform.position = hitboxLocation.position;
-            hitbox.transform.rotation = hitboxLocation.rotation;
-            transform.rotation = d.rotation;
+            if (this.transform.parent != null) // if currently holding, drop current wep
+            {
+                prevWep = this.transform;
+                drop();
+            }
 
+            //pickup another wep
+            if(this.transform != prevWep)
+                pickup();
         }
+    }
 
-        if (inputs.drop)  //G
-        {
-            this.transform.parent = null;
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().freezeRotation = false;
-            Debug.Log("Putting Down");
-            inputs.drop = false;
-            hitbox.transform.parent = transform;
-            hitbox.transform.position = Vector3.zero;
-            hitbox.transform.rotation = Quaternion.identity;
-            transform.rotation = Quaternion.identity;
-        }
+    void pickup() 
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().freezeRotation = true;
+        this.transform.position = d.position;
+        this.transform.parent = GameObject.Find("WepSlot").transform;
+        Debug.Log("PickingUp");
+        inputs.pickup = false;
+        hitbox.transform.parent = hitboxLocation;
+        hitbox.transform.position = hitboxLocation.position;
+        hitbox.transform.rotation = hitboxLocation.rotation;
+        transform.rotation = d.rotation;
+    }
+
+    void drop() 
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().freezeRotation = false;
+        Debug.Log("Putting Down");
+        inputs.drop = false;
+        hitbox.transform.parent = transform;
+        hitbox.transform.position = Vector3.zero;
+        hitbox.transform.rotation = Quaternion.identity;
+        transform.rotation = Quaternion.identity;
     }
 }
